1adb40e27ed562ca20be9ab5893c2ef393edf17c
bettergooddev <dev@bettergood.agency>
2025-06-06 16:58:32 -0400

    added text and logo

--- src/blocks/PlayingCards/Component.tsx
+++ src/blocks/PlayingCards/Component.tsx
@@ -13,14 +13,14 @@ export const PlayingCardsBlock: React.FC<PlayingCardsBlockProps> = async (props)
   const { heading, subheading } = props
 
   return (
-    <div className="relative py-16 [--card-height:20rem]">
+    <div className="relative py-16 [--card-height:22rem]">
       <div className="container">
         <Heading heading={heading} subheading={subheading} />
       </div>
 
       <MaskBackground shape={'wood'} innerClassName="relative flex justify-center ">
         <BounceCards
-          className="h-[calc(var(--card-height)+12rem)]"
+          className="h-[calc(var(--card-height)+9rem)]"
           elements={cards?.map((card) => (
             <PlayingCard key={card.id} card={card} className="!h-[var(--card-height)]" />
           ))}
--- src/blocks/PlayingCards/PlayingCard.tsx
+++ src/blocks/PlayingCards/PlayingCard.tsx
@@ -6,8 +6,16 @@ type CardType = NonNullable<Card['cards']>[number]
 function PlayingCard({ card, className }: { card: CardType; className?: string }) {
   return (
     <>
-      <div className={cn('theme-sugar-shack bg-background aspect-[5/7] h-[10rem]', className)}>
-        <Media resource={card.logo} className="size-32 object-contain" />
+      <div
+        className={cn(
+          'theme-pizza border-foreground border-[6px] rounded-xl overflow-hidden aspect-[5/7] h-[10rem]',
+          className,
+        )}
+      >
+        <div className="theme-sugar-shack bg-background size-full">
+          {card.logo && <Media resource={card.logo} className="size-32 object-contain" />}
+          <h2 className="type-h2">{card.name}</h2>
+        </div>
       </div>
     </>
   )
