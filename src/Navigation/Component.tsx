48ba15693452ba782dda9ae04fb1ff757a76abc7
bettergooddev <dev@bettergood.agency>
2025-07-07 19:55:01 -0400

    tweaking nav components

--- src/Navigation/Component.tsx
+++ src/Navigation/Component.tsx
@@ -5,11 +5,7 @@ import React from 'react'
 import type { Navigation } from '@/payload-types'
 
 export async function Navigation() {
-  // Check footer for how it used to grab this data and choose accordingly. claude insisteted on changing it into this but this was not the original approach.
-  // const navigationData = (await getCachedGlobal('navigation', 1)()) as Navigation
+  const navigationData = (await getCachedGlobal('navigation', 1)()) as Navigation
 
-  return (
-    <></>
-    // <NavigationClient data={navigationData} />
-  )
+  return <NavigationClient data={navigationData} />
 }
