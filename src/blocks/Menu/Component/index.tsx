0955d1aedb5da1cc81b1d3ae5eb6a5e0036f3f9d
bettergooddev <dev@bettergood.agency>
2025-07-09 11:27:19 -0400

    moved menu select to generic component

--- src/blocks/Menu/Component/index.tsx
+++ src/blocks/Menu/Component/index.tsx
@@ -1,13 +1,20 @@
+'use client'
+
 import type { MenuBlock as MenuBlockType, Menu } from '@/payload-types'
-import { MenuSelect } from './menu-select'
+import { IconSelect } from '../../../components/ui/icon-select'
+import { useEffect, useState } from 'react'
 
 export const MenuBlock: React.FC<MenuBlockType> = ({ heading, subheading, menus: menuProps }) => {
   const menus =
     menuProps?.filter((menu): menu is Menu => typeof menu === 'object' && menu !== null) || []
 
+  const [selectedMenuId, setSelectedMenuId] = useState<string | undefined>(menus[0]?.id)
+
   return (
     <div className="max-w-4xl mx-auto -mt-48" data-theme="pizza">
-      <MenuSelect menus={menus} />
+      {menus.length > 1 && (
+        <IconSelect items={menus} selected={selectedMenuId} onChange={setSelectedMenuId} />
+      )}
     </div>
   )
 }
