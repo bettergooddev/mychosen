6d65beff819e6a0c3aec76ca922647a0d6faf0d0
bettergooddev <dev@bettergood.agency>
2025-07-09 15:38:26 -0400

    i pushed the wrong file last

--- src/blocks/Features/Gallery/index.tsx
+++ src/blocks/Features/Gallery/index.tsx
@@ -17,7 +17,7 @@ const classes = {
     variants: {
       imageCount: {
         1: 'grid-cols-1',
-        2: 'grid-cols-1 md:grid-cols-2 max-w-3xl mx-auto',
+        2: 'grid-cols-1 md:grid-cols-2 md:max-w-3xl md:mx-auto',
         3: 'grid-cols-1 md:grid-cols-2 lg:grid-cols-3',
         4: 'grid-cols-1 md:grid-cols-2 lg:grid-cols-4',
       },
