1318a57b3fc4751d8d61cef72ae1a6f25632ec35
bettergooddev <dev@bettergood.agency>
2025-07-11 13:30:34 -0400

    added redirects

--- redirects.js
+++ redirects.js
@@ -12,7 +12,55 @@ const redirects = async () => {
     source: '/:path((?!ie-incompatible.html$).*)', // all pages except the incompatibility page
   }
 
-  const redirects = [internetExplorerRedirect]
+  // Domain redirects
+  const domainRedirects = [
+    {
+      source: '/:path*',
+      has: [
+        {
+          type: 'host',
+          value: 'www.mychosenpizza.com',
+        },
+      ],
+      destination: 'https://www.mychosencafe.com/pizza/:path*',
+      permanent: true,
+    },
+    {
+      source: '/:path*',
+      has: [
+        {
+          type: 'host',
+          value: 'mychosenpizza.com',
+        },
+      ],
+      destination: 'https://www.mychosencafe.com/pizza/:path*',
+      permanent: true,
+    },
+    {
+      source: '/:path*',
+      has: [
+        {
+          type: 'host',
+          value: 'www.mychosensugarshack.com',
+        },
+      ],
+      destination: 'https://www.mychosencafe.com/sugar-shack/:path*',
+      permanent: true,
+    },
+    {
+      source: '/:path*',
+      has: [
+        {
+          type: 'host',
+          value: 'mychosensugarshack.com',
+        },
+      ],
+      destination: 'https://www.mychosencafe.com/sugar-shack/:path*',
+      permanent: true,
+    },
+  ]
+
+  const redirects = [internetExplorerRedirect, ...domainRedirects]
 
   return redirects
 }
